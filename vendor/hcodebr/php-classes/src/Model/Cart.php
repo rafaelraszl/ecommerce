6a39a562e245a9b21eacbbb98be98a472635dcfc
Rafael Raszl <rafaelmsraszl@gmail.com>
2018-07-01 20:22:44 -0300

    Meus pedidos e Resumo carrinho

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -164,7 +164,7 @@ class Cart extends Model {
 		$results = $sql->select("
 			SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight,
 			SUM(vllength)AS vllength, SUM(vlweight) AS vlweight,
-			COUNT(*) AS nrqdt
+			COUNT(*) AS nrqtd
 			FROM tb_products a
 			INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct
 			WHERE b.idcart = :idcart AND dtremoved IS NULL;
@@ -188,7 +188,7 @@ class Cart extends Model {
 
 		$totals = $this->getProductsTotals();
 
-		if ($totals['nrqdt'] > 0) {
+		if ($totals['nrqtd'] > 0) {
 
 			if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
 			if ($totals['vllength'] < 16) $totals['vllength'] = 16;
